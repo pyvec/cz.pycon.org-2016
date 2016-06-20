12d1040e70d62f68d5eebba8a6b59aad51b0e89f
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2016-06-20 11:52:03 +0200

    Removed CMS context processors.

--- pyconcz_2016/pyconcz_2016/settings/__init__.py
+++ pyconcz_2016/pyconcz_2016/settings/__init__.py
@@ -65,8 +65,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'sekizai.context_processors.sekizai',
-                'cms.context_processors.cms_settings',
             ],
         },
     },
