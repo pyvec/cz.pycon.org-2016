d7f3e653d0818de8743cdfffca6e13839894ec3e
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2016-06-20 11:46:06 +0200

    Added draft of Code of Conduct.

--- pyconcz_2016/pyconcz_2016/urls.py
+++ pyconcz_2016/pyconcz_2016/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^about/code', TemplateView.as_view('pages/code.html')),
 ]
