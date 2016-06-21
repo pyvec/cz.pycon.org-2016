8bb54143e8779274a50c663b9ebe7ef199a2cf77
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2016-06-21 10:29:57 +0200

    Prefix urls with year and add defautl redirect.

--- pyconcz_2016/pyconcz_2016/urls.py
+++ pyconcz_2016/pyconcz_2016/urls.py
@@ -1,13 +1,18 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 
-urlpatterns = [
+prefixed_urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', TemplateView.as_view(template_name='pages/homepage.html')),
     url(r'^about/cfp', TemplateView.as_view(template_name='pages/cfp.html')),
     url(r'^about/code', TemplateView.as_view(template_name='pages/code.html')),
     url(r'^sponsors/offer', TemplateView.as_view(template_name='pages/sponsors_offer.html')),
+]
+
+urlpatterns = [
+    url(r'^2016/', include(prefixed_urlpatterns)),
+    url(r'^$', RedirectView.as_view(url='/2016/'))
 ]
