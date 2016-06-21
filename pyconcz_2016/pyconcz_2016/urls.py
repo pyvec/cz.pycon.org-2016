c5ece5e8960d5084e80f148a391d93d6223249a9
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2016-06-21 10:14:29 +0200

    Added two more static pages needed for launch.

--- pyconcz_2016/pyconcz_2016/urls.py
+++ pyconcz_2016/pyconcz_2016/urls.py
@@ -6,6 +6,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
+    url(r'^$', TemplateView.as_view(template_name='pages/homepage.html')),
     url(r'^about/cfp', TemplateView.as_view(template_name='pages/cfp.html')),
     url(r'^about/code', TemplateView.as_view(template_name='pages/code.html')),
+    url(r'^sponsors/offer', TemplateView.as_view(template_name='pages/sponsors_offer.html')),
 ]
