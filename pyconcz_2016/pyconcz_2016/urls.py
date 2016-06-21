be3a6f9daa787f9896b6086d9836c5fa4006db6f
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2016-06-21 10:31:57 +0200

    Don't prefix admin urls.

--- pyconcz_2016/pyconcz_2016/urls.py
+++ pyconcz_2016/pyconcz_2016/urls.py
@@ -4,8 +4,6 @@ from django.views.generic import TemplateView, RedirectView
 
 
 prefixed_urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-
     url(r'^$', TemplateView.as_view(template_name='pages/homepage.html')),
     url(r'^about/cfp', TemplateView.as_view(template_name='pages/cfp.html')),
     url(r'^about/code', TemplateView.as_view(template_name='pages/code.html')),
@@ -14,5 +12,6 @@ prefixed_urlpatterns = [
 
 urlpatterns = [
     url(r'^2016/', include(prefixed_urlpatterns)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', RedirectView.as_view(url='/2016/'))
 ]
