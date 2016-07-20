1a4f03ef5e502a16d3942b06b91fd2dcb3f1dff6
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2016-07-20 18:47:04 +0200

    Use postres in development

--- pyconcz_2016/settings_dev.py
+++ pyconcz_2016/settings_dev.py
@@ -6,3 +6,14 @@ SECRET_KEY = 42
 INTERNAL_IPS = ['127.0.0.1']
 CSRF_COOKIE_SECURE = False
 SESSION_COOKIE_SECURE = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
