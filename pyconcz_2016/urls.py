16a1c66e243bb51fe673ada8ca664347cb2704f9
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2016-07-21 09:38:39 +0200

    Added about page

--- pyconcz_2016/urls.py
+++ pyconcz_2016/urls.py
@@ -8,7 +8,7 @@ prefixed_urlpatterns = [
 
     # static pages
     url(r'^$', TemplateView.as_view(template_name='pages/homepage.html')),
-    url(r'^about/cfp', TemplateView.as_view(template_name='pages/cfp.html')),
+    url(r'^about', TemplateView.as_view(template_name='pages/about.html')),
     url(r'^about/code', TemplateView.as_view(template_name='pages/code.html')),
     url(r'^sponsors/offer', TemplateView.as_view(template_name='pages/sponsors_offer.html')),
 ]
