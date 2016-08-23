bfd30bac60cfc9972346ac4c4a04ca511518a8a0
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2016-08-23 07:44:03 +0200

    Make email field private and optional

--- pyconcz_2016/team/models.py
+++ pyconcz_2016/team/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class Organizer(models.Model):
     full_name = models.CharField(max_length=200)
-    email = models.EmailField()
+    email = models.EmailField(
+        default='', blank=True,
+        help_text="This is private")
     bio = models.TextField()
     twitter = models.CharField(max_length=255, blank=True)
     github = models.CharField(max_length=255, blank=True)
