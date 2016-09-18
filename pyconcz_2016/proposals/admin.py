e61995e14f84b577e523dec5ded2a12a2989df82
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2016-09-18 19:41:36 +0200

    Generate Proposal objects which holds scores

--- pyconcz_2016/proposals/admin.py
+++ pyconcz_2016/proposals/admin.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib import messages
+from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import redirect
+
+from pyconcz_2016.proposals.models import Proposal
 
 
 class EntryAdmin(admin.ModelAdmin):
@@ -31,5 +35,28 @@ class EntryAdmin(admin.ModelAdmin):
         )
 
         info = self.model._meta.app_label, self.model._meta.model_name
+
+        ct = ContentType.objects.get_for_model(self.model)
+        existing_ids = (
+            Proposal.objects
+                .filter(content_type=ct)
+                .values_list('id', flat=True)
+        )
+
+        proposals = (
+            Proposal(content_type=ct, object_id=oid) for oid in
+            self.model.objects
+                .exclude(id__in=existing_ids)
+                .values_list('id', flat=True)
+        )
+
+        objs = Proposal.objects.bulk_create(proposals)
+
+        obj_count = len(objs)
+        if obj_count:
+            msg = "{} new proposals available for scoring"
+            messages.success(request, msg.format(obj_count))
+        else:
+            messages.info(request, "No new proposals available for scoring")
 
         return redirect('admin:%s_%s_changelist' % info)
