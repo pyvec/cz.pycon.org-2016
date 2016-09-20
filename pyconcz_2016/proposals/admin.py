4d50853c58de56e279896b81adc2df909136631c
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2016-09-20 15:26:54 +0200

    Remove extra newline

--- pyconcz_2016/proposals/admin.py
+++ pyconcz_2016/proposals/admin.py
@@ -54,7 +54,6 @@ class EntryAdmin(admin.ModelAdmin):
             if field.name not in ['id', 'rankings']
         ]
 
-
     def get_urls(self):
         info = self.model._meta.app_label, self.model._meta.model_name
 
