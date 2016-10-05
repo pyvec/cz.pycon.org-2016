d30a615aa583c2724e726be1b721955cd1b8ffe4
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2016-10-05 13:01:32 +0200

    Remove debug string from templates

--- pyconcz_2016/settings.py
+++ pyconcz_2016/settings.py
@@ -74,7 +74,6 @@ TEMPLATES = [
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
-            'string_if_invalid': '%s',
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
