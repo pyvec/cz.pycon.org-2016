7d55968b83698acb7ee1ec8e36437607fe1f1860
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2016-10-09 21:53:26 +0200

    Added speakers schedule to link

--- pyconcz_2016/speakers/urls.py
+++ pyconcz_2016/speakers/urls.py
@@ -4,5 +4,5 @@ from pyconcz_2016.speakers.views import speakers_list, talks_timeline
 
 urlpatterns = [
     url('^(?P<type>(talks|workshops))$', speakers_list, name="speakers_list"),
-    url('^timeline$', talks_timeline, name="talks_timeline"),
+    url('^schedule$', talks_timeline, name="speakers_schedule"),
 ]
